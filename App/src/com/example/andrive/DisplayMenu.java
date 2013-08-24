8e51bde9be6dfb3d092fe8a65bc18d1c9183626e
Alfonso Ros <alfonsoros88@gmail.com>
2013-08-25 01:53:26 +0200

    Create android project

---
+++ App/src/com/example/andrive/DisplayMenu.java
@@ -0,0 +1,15 @@
+package com.example.andrive;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DisplayMenu extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-andrive
+Andrive
 =======
 
 Andrive will be an Android application which uses the phone camera to measure distances to near vehicles while driving and other metrics
